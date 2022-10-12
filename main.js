348a7ae7f427bd3582d4d2162fe65eb94c3864f5
galuz1234 <galuz2@hotmail.com>
2022-10-12 09:05:38 -0500

    avance calculadora

---
+++ main.js
@@ -0,0 +1,91 @@
+var IngresaDato = "0";
+var numero1 = "0";
+var numero2 = "0";
+var resultado = "0";
+var operacion = "";
+
+var DatoIngresado = document.getElementById(IngresaDato); /* los datos que se ingresen en el input se guardaran en la variable inicializada */
+
+
+document.getElementById('nro7').onclick = function changeContent () {
+    DatoIngresado.value = "7";
+}
+
+document.getElementById('nro8').onclick = function changeContent () {
+    DatoIngresado.value = "8";
+}
+
+document.getElementById('nro9').onclick = function changeContent () {
+    DatoIngresado.value = "9";
+}
+
+document.getElementById('signo*').onclick = function changeContent () {
+    DatoIngresado.value = "*";
+}
+
+document.getElementById('nro4').onclick = function changeContent () {
+    DatoIngresado.value = "4";
+}
+
+document.getElementById('nro5').onclick = function changeContent () {
+    DatoIngresado.value = "5";
+}
+
+document.getElementById('nro6').onclick = function changeContent () {
+    DatoIngresado.value = "6";
+}
+
+document.getElementById('signo-').onclick = function changeContent () {
+    DatoIngresado.value = "-";
+}
+
+document.getElementById('nro1').onclick = function changeContent () {
+    DatoIngresado.value = "1";
+}
+
+document.getElementById('nro2').onclick = function changeContent () {
+    DatoIngresado.value = "2";
+}
+
+document.getElementById('nro3').onclick = function changeContent () {
+    DatoIngresado.value = "3";
+}
+
+document.getElementById('signo+').onclick = function changeContent () {
+    DatoIngresado.value = "+";
+}
+
+document.getElementById('nro7').onclick = function changeContent () {
+    DatoIngresado.value = "7";
+}
+
+document.getElementById('nro8').onclick = function changeContent () {
+    DatoIngresado.value = "8";
+}
+
+document.getElementById('nro9').onclick = function changeContent () {
+    DatoIngresado.value = "9";
+}
+
+document.getElementById('signo*').onclick = function changeContent () {
+    DatoIngresado.value = "*";
+}
+
+document.getElementById('del').onclick = function changeContent () {
+    DatoIngresado.value = " ";
+}
+
+document.getElementById('nro0').onclick = function changeContent () {
+    DatoIngresado.value = "0";
+}
+
+document.getElementById('signo,').onclick = function changeContent () {
+    DatoIngresado.value = ",";
+}
+
+document.getElementById('res').onclick = function changeContent () {
+    DatoIngresado.value = "=";
+}
+
+
+switch 
